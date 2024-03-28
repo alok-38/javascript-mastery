acb26fe7b63a31fe2860ec7d0e3ee194637048e6
Alokananda Y <plantsfortrees@gmail.com>
2024-03-28 15:35:16 +0530

    props

--- understanding-props/src/App.js
+++ understanding-props/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Tweet from './components/Tweet';
+import CreateTweet from './components/CreateTweet';
 
 function App() {
+  const name = "Alok";
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello React!</h1>
+        <CreateTweet />
+        <Tweet author="Brad Traversy" />
       </header>
     </div>
   );
