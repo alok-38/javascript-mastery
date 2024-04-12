5fd50b2ebc60203faf1bf946fb6148c12fc7ac22
Alokananda Y <plantsfortrees@gmail.com>
2024-04-12 19:23:07 +0530

    js

---
+++ javaScript-prerequisites/array-methods/app.js
@@ -0,0 +1,7 @@
+const itemList = document.getElementById('itemList');
+const itemInput = document.getElementById('itemInput');
+const addItemBtn = document.getElementById('addItemBtn');
+const filterInput = document.getElementById('filterInput');
+
+let items = [];
+
