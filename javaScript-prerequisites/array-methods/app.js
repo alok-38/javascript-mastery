48a9cc90398d9f80de8b8f78354f4e4d6fd55812
Alokananda Y <plantsfortrees@gmail.com>
2024-04-23 14:23:30 +0530

    Getting started

--- javaScript-prerequisites/array-methods/app.js
+++
@@ -1,7 +0,0 @@
-const itemList = document.getElementById('itemList');
-const itemInput = document.getElementById('itemInput');
-const addItemBtn = document.getElementById('addItemBtn');
-const filterInput = document.getElementById('filterInput');
-
-let items = [];
-
