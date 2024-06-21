af475149a08a600885e06d2211b8c77587105808
Alokananda Y <plantsfortrees@gmail.com>
2024-06-21 07:09:06 +0530

    Layout wrapper

---
+++ corinna-ai/src/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = async ({ children }: Props) => {
+  const user = await currentUser();
+
+  if (user) redirect("/");
+  return <div>layout</div>;
+};
+
+export default Layout;
