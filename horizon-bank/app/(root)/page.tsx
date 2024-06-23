1d961aace4137f9327ab543259a83cea2ed0dfef
Alokananda Y <plantsfortrees@gmail.com>
2024-06-23 08:50:33 +0530

    Total Current Balance

--- horizon-bank/app/(root)/page.tsx
+++ horizon-bank/app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
   return (
-	<div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
