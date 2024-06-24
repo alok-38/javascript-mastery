13d938c9c1a77b919f8da524bbb4ab9fd5982bb6
Alokananda Y <plantsfortrees@gmail.com>
2024-06-24 09:24:56 +0530

    Navbar

---
+++ next14-tutorial/src/app/admin/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./admin.module.css";
+
+const AdminPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.col}></div>
+        <div className={styles.col}>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.col}></div>
+        <div className={styles.col}>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
--- next14-tutorial/src/app/layout.js
+++ next14-tutorial/src/app/layout.js
@@ -1,4 +1,7 @@
+import Footer from "@/components/footer/Footer";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
