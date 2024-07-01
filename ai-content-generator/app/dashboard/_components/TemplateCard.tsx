8d03bdccfef3ede2953f735c630b0d0008c84dd6
Alokananda Y <plantsfortrees@gmail.com>
2024-07-01 11:43:12 +0530

    grid cols

--- ai-content-generator/app/dashboard/_components/TemplateCard.tsx
+++ ai-content-generator/app/dashboard/_components/TemplateCard.tsx
@@ -22,10 +22,11 @@ export interface TEMPLATE {
 // Use 'item' as a prop with type 'TEMPLATE'
 const TemplateCard = ({ item }: { item: TEMPLATE }) => {
   return (
-    <div className="p-5 shadow-md rounded-md border bg-white flex flex-col">
+    <div className="p-5 shadow-md rounded-md border bg-white flex flex-col gap-3
+    cursor-pointer">
 		<Image src={item.icon} alt="icon" width={50} height={50} />
-		<h2>{item.name}</h2>
-    <p>{item.description}</p>
+		<h2 className="font-medium text-lg">{item.name}</h2>
+    <p className="text-slate-500 line-clamp-3">{item.description}</p>
     </div>
   );
 };
--- ai-content-generator/app/dashboard/_components/TemplateListSection.tsx
+++ ai-content-generator/app/dashboard/_components/TemplateListSection.tsx
@@ -22,7 +22,7 @@ interface TEMPLATE {
 
 const TemplateListSection = () => {
   return (
-    <div>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5">
       {Templates.map((item: TEMPLATE, index: number) => (
         <div key={index}>
           <TemplateCard item={item} /> {/* Pass 'item' as prop */}
