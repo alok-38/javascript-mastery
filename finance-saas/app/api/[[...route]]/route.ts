3186855e7099a84bc7c21c2a5b8f5dacb0dd5af8
Alokananda Y <plantsfortrees@gmail.com>
2024-07-02 09:33:31 +0530

    Clerk middleware

--- finance-saas/app/api/[[...route]]/route.ts
+++ finance-saas/app/api/[[...route]]/route.ts
@@ -1,15 +1,51 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { error } from "console";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
+app
+  .get("/hello", clerkMiddleware(), (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" });
+    }
+    return c.json({
+      message: "Hello Next.js!",
+      userId: auth.userId,
+    });
   })
-})
+  .get("/hello/:test", (c) => {
+    return c.json({
+      message: "Hello World",
+    });
+  })
+  .post(
+    "/create/:postId",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+        userId: z.number(),
+      })
+    ),
+    zValidator(
+      "param",
+      z.object({
+        postId: z.number(),
+      })
+    ),
+    (c) => {
+      const { name, userId } = c.req.valid("json");
+      const { postId } = c.req.valid("param");
+      return c.json({});
+    }
+  );
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
